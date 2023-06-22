e395b5a3f7517f5d335749b3564545266f62d828
Marta Gajewska <gajewska.marta16@gmail.com>
2023-06-22 14:46:43 +0200

    add sidebar

--- animal-rescue/src/components/Navbar.js
+++ animal-rescue/src/components/Navbar.js
@@ -1,5 +1,106 @@
+import styled from 'styled-components';
+import { FaBars, FaPaw } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom';
+import { links } from '../utils/constants';
+import { useSidebarContext } from '../context/sidebar_context';
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  const {openSidebar} = useSidebarContext()
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <div className="nav-header">
+          <IconContext.Provider value={{ className: 'icon' }}>
+            <Link to="/">
+              <FaPaw />
+            </Link>
+          </IconContext.Provider>
+          <button type="button" className="nav-toggle" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.nav`
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .icon {
+      color: var(--clr-primary-1);
+      font-size: 3.5rem;
+      &:hover {
+        color: var(--clr-primary-3)
+      }
+    }
+  }
+  .nav-toggle {
+    background: transparent;
+    border: transparent;
+    color: var(--clr-primary-1);
+    cursor: pointer;
+    svg {
+      font-size: 2rem;
+    }
+  }
+  .nav-links {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 0.5rem;
+      }
+      a:link,
+      a:visited {
+        color: var(--clr-primary-1);
+        font-size: 1rem;
+        text-transform: capitalize;
+        letter-spacing: var(--spacing);
+        padding: 0.5rem;
+        &:hover,
+        &:active {
+          border-bottom: 2px solid var(--clr-primary-3);
+        }
+      }
+    }
+  }
+`;
 
 export default Navbar;
--- animal-rescue/src/components/Sidebar.js
+++ animal-rescue/src/components/Sidebar.js
@@ -1,5 +1,106 @@
+import styled from 'styled-components';
+import { FaTimes, FaPaw } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom';
+import { links } from '../utils/constants';
+import { useSidebarContext } from '../context/sidebar_context';
+
 const Sidebar = () => {
-  return <h1>Sidebar</h1>;
+  const { isSidebarOpen, closeSidebar } = useSidebarContext();
+  return (
+    <Wrapper>
+      <aside
+        className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}
+      >
+        <div className="sidebar-header">
+          <IconContext.Provider value={{ className: 'icon' }}>
+            <FaPaw />
+          </IconContext.Provider>
+          <button type="button" className="close-btn" onClick={closeSidebar}>
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {links.map(({ id, text, url }) => {
+            return (
+              <li key={id}>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </aside>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  text-align: center;
+  .sidebar-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 1.5rem;
+  }
+  .icon {
+    justify-self: center;
+    font-size: 2rem;
+  }
+  .close-btn {
+    font-size: 2rem;
+    background: transparent;
+    border-color: transparent;
+    color: var(--clr-primary-1);
+    transition: var(--transition);
+    cursor: pointer;
+    margin-top: 0.2rem;
+  }
+  .close-btn:hover {
+    color: var(--clr-primary-3);
+  }
+  .links {
+    margin-bottom: 2rem;
+  }
+  a:link,
+  a:visited {
+    display: block;
+    text-align: left;
+    font-size: 1rem;
+    text-transform: capitalize;
+    padding: 1rem 1.5rem;
+    color: var(--clr-primary-1);
+    transition: var(--transition);
+    letter-spacing: var(--spacing);
+  }
+  a:hover,
+  a:active {
+    padding: 1rem 1.5rem;
+    padding-left: 2rem;
+    background: var(--clr-grey-10);
+    color: var(--clr-primary-1);
+  }
+
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--clr-white);
+    transition: var(--transition);
+    transform: translate(-100%);
+    z-index: -1;
+  }
+  .show-sidebar {
+    transform: translate(0);
+    z-index: 999;
+  }
+  @media screen and (min-width: 992px) {
+    .sidebar {
+      display: none;
+    }
+`;
 
 export default Sidebar;
